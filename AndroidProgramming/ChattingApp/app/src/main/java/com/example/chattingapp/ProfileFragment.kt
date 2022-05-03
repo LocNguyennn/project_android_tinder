ec78fd63e094c10226f87f0c2e85438505f49861
LocNguyennn <nguyentanloc046@gmail.com>
2022-05-03 20:10:07 +0700

    fix sign in layout + profile
    sign in layout still need to use textinputlayout to have hide/unhide password. Will update later

--- AndroidProgramming/ChattingApp/app/src/main/java/com/example/chattingapp/ProfileFragment.kt
+++ AndroidProgramming/ChattingApp/app/src/main/java/com/example/chattingapp/ProfileFragment.kt
@@ -1,5 +1,6 @@
 package com.example.chattingapp
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -9,11 +10,13 @@ import com.example.chattingapp.databinding.FragmentHomeBinding
 import com.example.chattingapp.databinding.FragmentProfileBinding
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
+import com.google.firebase.database.*
 import com.google.firebase.ktx.Firebase
 
 class ProfileFragment : Fragment() {
-    private lateinit var binding : FragmentProfileBinding
-    private lateinit var mAuth : FirebaseAuth
+    private lateinit var binding: FragmentProfileBinding
+    private lateinit var mAuth: FirebaseAuth
+    private lateinit var mDbRef: DatabaseReference
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
     }
@@ -24,14 +27,22 @@ class ProfileFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         mAuth = FirebaseAuth.getInstance()
-        binding = FragmentProfileBinding.inflate(inflater,container,false)
+        mDbRef = FirebaseDatabase.getInstance().getReference()
+        binding = FragmentProfileBinding.inflate(inflater, container, false)
         return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        val currentUser = mAuth.currentUser as User
-        binding.txtFullName.text = currentUser.name.toString()
-        binding.txtUserName.text = currentUser.email.toString()
+        findUser()
 
+    }
+
+    private fun findUser() {
+        mDbRef.child("user").child(mAuth.currentUser!!.uid).get().addOnSuccessListener {
+            if(it.exists()){
+                binding.txtFullName.text = it.child("name").value.toString()
+                binding.txtUserName.text = it.child("email").value.toString()
+            }
+        }
     }
 }
